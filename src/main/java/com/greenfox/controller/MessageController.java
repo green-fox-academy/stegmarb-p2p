6d5dd256c9e6bea8904af93f6ed9e486d96bf52a
Balázs Stégmár <Balzs Stgmr>
2017-05-17 11:04:02 +0200

    feat(PeerToPeer): app has repository for storing messages

--- src/main/java/com/greenfox/controller/MessageController.java
+++ src/main/java/com/greenfox/controller/MessageController.java
@@ -1,10 +1,16 @@
 package com.greenfox.controller;
 
+import com.greenfox.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MessageController {
+
+  @Autowired
+  MessageRepository messageRepository;
+
   @RequestMapping("/")
   public String indexSite() {
     return "index";
---
+++ src/main/java/com/greenfox/repository/MessageRepository.java
@@ -0,0 +1,7 @@
+package com.greenfox.repository;
+
+import com.greenfox.model.Message;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessageRepository extends CrudRepository<Message, Long> {
+}
