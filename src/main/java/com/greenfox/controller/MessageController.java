4efb947bbf5fe396ad3903106b8f95787fb1c9c2
Balázs Stégmár <Balzs Stgmr>
2017-05-17 23:23:35 +0200

    feat(PeerToPeer): app indicates if no username were given

--- src/main/java/com/greenfox/controller/MessageController.java
+++ src/main/java/com/greenfox/controller/MessageController.java
@@ -6,10 +6,11 @@ import com.greenfox.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Controller
 public class MessageController {
@@ -27,13 +28,19 @@ public class MessageController {
 
   @RequestMapping("/enter")
   public String enterPage(Model model) {
-    model.addAttribute("currentUser", user.findAll());
+    List<User> users = new ArrayList<>((Collection) user.findAll());
+    model.addAttribute("currentUser", users);
     return "enter";
   }
 
   @RequestMapping(value = "/enter", method = RequestMethod.POST)
-  public String enterMessage(@RequestParam("username") String user) {
+  public String enterMessage(Model model, @RequestParam("username") String user) {
+    if (user == "") {
+      model.addAttribute("error", "The username field is empty");
+      return "enter";
+    } else {
     this.user.save(new User(user));
-    return "redirect:/";
+      return "redirect:/";
   }
+ }
 }
--- src/main/java/com/greenfox/model/User.java
+++ src/main/java/com/greenfox/model/User.java
@@ -12,6 +12,10 @@ public class User {
   private long id;
   private String username;
 
+  public User() {
+
+  }
+
   public User(String username) {
     this.username = username;
   }
